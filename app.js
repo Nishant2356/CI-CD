85b7fb0b61df028a62b5670384bc1c280fc9b8fd
Nishant2356 <nm2356opp@gmail.com>
2026-03-31 15:41:49 +0530

    check2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.get("/", (req, res) => {
-  res.send("CI/CD working for AspireX🚀");
+  res.send("CI/CD working🚀");
 });
 
 // Health check (important for deployment platforms)
